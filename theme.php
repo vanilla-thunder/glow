ce3bd78feded7a0a4e33cefce30beca9e6ef6c7a
Marat <mb@bestlife.ag>
2016-10-17 17:29:27 +0200

    fix

--- theme.php
+++ theme.php
@@ -1,12 +1,12 @@
 <?php
 
-$aTheme = array(
+$aTheme = [
     'id'           => 'glow',
     'title'        => 'Glow',
     'description'  => 'cleaned, updated and bugfixed flow theme for oxid eshop 4.9 / 4.10',
     'thumbnail'    => 'theme.jpg',
     'version'      => '0.1.0',
     'parentTheme'  => 'flow',
-    'parentVersion'=> '1.0.0-beta.3',
+    'parentVersions'=> ['1.0.0-beta.2','1.0.0-beta.3'],
     'author'       => 'Marat Bedoev',
-);
+];
